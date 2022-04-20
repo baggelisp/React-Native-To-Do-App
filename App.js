b2ecb44cd6aea4c71024b095634119e0bc46deb6
Vangelis Spathonis <baggelisp.keph@gmail.com>
2022-04-20 11:15:45 +0300

    style add task

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Platform, TouchableWithoutFeedback, Button, KeyboardAvoidingView, TextInput} from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
 import Task from './components/Task';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Today's tasks */}
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>What's up, Joy!</Text>
         <View style={styles.items}>
@@ -14,6 +16,24 @@ export default function App() {
 
         </View>
       </View>
+
+      {/* White a task */}
+
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.writeTaskWrapper}
+      >
+        <TextInput placeholder="Write a task!" style={styles.textInput} />
+
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+
+        </TouchableOpacity>
+        
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -33,5 +53,36 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  textInput: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  }, 
+  addText: {
+    fontSize: 24,
+    color: '#C0C0C0'
   }
 });
