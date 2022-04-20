7975218d2877962750381a896d060f7e1dd4c878
Vangelis Spathonis <baggelisp.keph@gmail.com>
2022-04-20 11:04:33 +0300

    changes on task

--- components/Task.js
+++ components/Task.js
@@ -5,10 +5,10 @@ function Task(props) {
   return (
     <View style={styles.item}>
         <View style={styles.itemLeft}>
-            <TouchableOpacity style={styles.square}></TouchableOpacity>
+            <TouchableOpacity style={styles.circular}></TouchableOpacity>
             <Text style={styles.itemText}>{props.text}</Text>
         </View>
-        <View style={styles.circular}></View>
+        {/* <View style={styles.circular}></View> */}
     </View>
   )
 }
@@ -31,22 +31,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexWrap: 'wrap'
     },
-    square: {
+    circular: {
         width: 24,
         height: 24,
-        backgroundColor: '#55BCF6',
-        opacity: 0.4,
-        borderRadius: 5,
         marginRight: 15,
+        borderColor: '#55BCF6',
+        borderWidth: 3,
+        borderRadius: 12,
     },
     itemText: {
         maxWidth: '80%',
-    },
-    circular: {
-        width: 12,
-        height: 12,
-        borderColor: '#55BCF6',
-        borderWidth: 2,
-        borderRadius: 5,
     }
 });
